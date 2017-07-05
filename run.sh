5e7295f7bebfef225a7c8eb2654630cf74710816
darkmarbo <darkmarbo@163.com>
2017-07-05 17:13:27 +0800

    v4 整个流程ok

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 if(($# < 1));then
     echo "usage $0 in_file "
+    echo "例如:  ./run.sh text.txt "
     exit 0;
 fi
 
@@ -11,7 +12,14 @@ rm -rf full && mkdir -p full
 
 dos2unix ${in_file}
 
-python getProcessedFormat.py  ${in_file}  ${in_file}.pro
+
+######## 为hts中label做准备
+awk -F"\t" '{print $2}'   ${in_file}   |\
+	sed 's/，//g;s/。//g;s/！//g;s/（//g;s/）//g;s/——//g;s/、//g;s/【//g;s/】//g;s/”//g;s/“//g;s/‘//g;s/’//g;s/：//g;s/；//g;s/？//g;s/、//g;s/》//g;s/《//g;s/……//g;s/-//g'|\
+	sed 's/#4//g;s/#1/ /g;s/#2/|/g;s/#3/$/g'\
+>  ${in_file}.awk
+
+python getProcessedFormat.py  ${in_file}.awk  ${in_file}.pro
 
 ./Ttslabel ${in_file}.pro 
 
